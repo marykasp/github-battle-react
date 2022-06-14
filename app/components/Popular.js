665e21743c7d9903305c93f9c34d09f34a832d3f
Mary Kasparian <kasparian.mary@gmail.com>
2022-06-14 13:07:02 -0500

    Install PropTypes set for LanguagesNav functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
+// Functional Component - without Hooks can be used to be passed props and render a UI component
+function LanguagesNav({ selected, updateLanguage}) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+  return (
+    <ul className='flex-center'>
+      {/* map over the languages array and create a list item for each */}
+    {languages.map((language) => (
+      <li key={language}>
+        <button
+          className='btn-clear nav-link'
+          style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+          onClick={() => updateLanguage(language)}>
+          {language}
+        </button>
+      </li>
+    ))}
+  </ul>
+  )
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  updateLanguage: PropTypes.func.isRequired
+}
+
+// Class Component
 class Popular extends React.Component {
   constructor(props) {
     super(props)
@@ -19,21 +46,14 @@ class Popular extends React.Component {
   }
 
   render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
     return (
-      <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={language}>
-            <button
-              className='btn-clear nav-link'
-              style={language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null}
-              onClick={() => this.updateLanguage(language)}>
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <>
+        <LanguagesNav
+          selected={this.state.selectedLanguage}
+          updateLanguage={this.updateLanguage}
+        />
+      </>
     )
   }
 }
