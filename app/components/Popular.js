2101e66062261ef0525b1b1716ca63d8dfdfc553
Mary Kasparian <kasparian.mary@gmail.com>
2022-06-20 07:24:28 -0500

    Install react icons

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,96 +1,95 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import { fetchPopularRepos } from '../utils/api';
+import React from "react";
+import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
 
 // Functional Component - without Hooks can be used to be passed props and render a UI component
-function LanguagesNav({ selected, updateLanguage}) {
-  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+function LanguagesNav({ selected, updateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
   return (
-    <ul className='flex-center'>
+    <ul className="flex-center">
       {/* map over the languages array and create a list item for each */}
-    {languages.map((language) => (
-      <li key={language}>
-        <button
-          className='btn-clear nav-link'
-          style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
-          onClick={() => updateLanguage(language)}>
-          {language}
-        </button>
-      </li>
-    ))}
-  </ul>
-  )
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+            onClick={() => updateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
-  updateLanguage: PropTypes.func.isRequired
-}
+  updateLanguage: PropTypes.func.isRequired,
+};
 
 // Class Component
 class Popular extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       selectedLanguage: "All",
       // repos is an object with the selected language as keys point to the array of repos returned from the fetch request
       repos: {},
-      error: null
-    }
+      error: null,
+    };
 
     this.updateLanguage = this.updateLanguage.bind(this);
-    this.isLoading = this.isLoading.bind(this)
+    this.isLoading = this.isLoading.bind(this);
   }
 
   // will invoke the fetch request once teh component mounts
   componentDidMount() {
-    this.updateLanguage(this.state.selectedLanguage)
+    this.updateLanguage(this.state.selectedLanguage);
   }
 
   // update local state of component - causes a re-render
-  updateLanguage (selectedLanguage) {
+  updateLanguage(selectedLanguage) {
     // only need to set the language and error state
     this.setState({
       selectedLanguage,
-      error: null
-    })
+      error: null,
+    });
 
     // only fetch repos if repos does not have that language property
-    if(!this.state.repos[selectedLanguage]) {
-
+    if (!this.state.repos[selectedLanguage]) {
       fetchPopularRepos(selectedLanguage)
         .then((data) => {
           // use previous data on repos state object - pass this.setState a callback function
-          this.setState(({repos}) => ({
+          this.setState(({ repos }) => ({
             repos: {
               // pass in all the properties of the repos object
               ...repos,
               // dynamic setting of a new key:value pair - key is the language, data is the array of repos returned from fetch request
-              [selectedLanguage]: data
-            }
-          }))
+              [selectedLanguage]: data,
+            },
+          }));
         })
         .catch(() => {
-          console.warn('Error fetching repos:', error)
+          console.warn("Error fetching repos:", error);
 
           this.setState({
-            error: 'There was an error fetching repositories'
-          })
-        })
+            error: "There was an error fetching repositories",
+          });
+        });
       // make fetch request to Github API - returns an array of objects
-
     }
-
   }
 
+  // funnctionn that will determine whether fetching is still occurring
   isLoading() {
-    const {selectedLanguage, repos, error} = this.state;
-    return !repos[selectedLanguage] && error === null
+    const { selectedLanguage, repos, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
   }
 
   render() {
-    const {selectedLanguage, repos, error} = this.state
+    const { selectedLanguage, repos, error } = this.state;
     return (
       <>
         <LanguagesNav
@@ -103,9 +102,11 @@ class Popular extends React.Component {
 
         {error && <p>{error}</p>}
 
-        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+        {repos[selectedLanguage] && (
+          <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>
+        )}
       </>
-    )
+    );
   }
 }
 
