584f22c6324778282d9333f54749e8de4028a928
Mary Kasparian <kasparian.mary@gmail.com>
2022-06-20 09:53:02 -0500

    Render PlayInput class component, controlled componennt, input state controlled by component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaUserFriends, FaFighterJet, FaTrophy, FaUser } from "react-icons/fa";
+import PropTypes from "prop-types";
 
 function Instructions() {
   return (
@@ -27,11 +28,71 @@ function Instructions() {
   );
 }
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+    };
+    // bind this keyword to referencce this componnent
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // passs the state to a function
+    this.props.onSubmit(this.state.username);
+  }
+
+  // updates the state of the PlayerInput component - changinng the value of the input field text
+  handleChange(event) {
+    // updates local state, causes re-render, input field is updated
+    this.setState({ username: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="column player">
+        <label htmlFor="username" className="player-label">
+          {this.props.label}
+        </label>
+        <div className="row player-inputs">
+          <input
+            type="text"
+            id="username"
+            className="input-light"
+            placeholder="github username"
+            autoComplete="off"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            type="submit"
+            className="btn dark-btn"
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
 export default class Battle extends React.Component {
   render() {
     return (
       <>
         <Instructions />
+        <PlayerInput label="Label" onSubmit={(value) => console.log(value)} />
       </>
     );
   }
