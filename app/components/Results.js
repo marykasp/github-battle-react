1ebfad7c97425abba6102ef733cd955bc6b56d51
Mary Kasparian <kasparian.mary@gmail.com>
2022-06-22 18:28:39 -0500

    Add state to Results class component, player1 & player 2 object results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,20 +1,44 @@
 import React from "react";
 import { battle } from "../utils/api";
 
+// when component first mounts or renders call the battle fetch function - invokes Promise.all fetch requests to returns an object for each player containing profile info and score - since returning Promise.all returns a promise so need to call .then() to get the resolved value of the promise which is an array of objects
 export default class Results extends React.Component {
-  // when component first mounts or renders call the battle fetch function - invokes Promise.all fetch requests to returns an object for each player containing profile info and score - since returning Promise.all returns a promise so need to call .then() to get the resolved value of the promise which is an array of objects
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    };
+  }
   componentDidMount() {
     const { playerOne, playerTwo } = this.props;
     // invoke battle function
-    battle([playerOne, playerTwo]).then((players) => {
-      console.log(`data`, players);
-    });
+    battle([playerOne, playerTwo])
+      .then((players) => {
+        // console.log(`data`, players);
+        // update component state
+        this.setState({
+          winner: players[0],
+          loser: players[1],
+          error: null,
+          loading: false,
+        });
+      })
+      .catch(({ message }) => {
+        this.setState({
+          error: message,
+          loading: false,
+        });
+      });
   }
   render() {
     return (
       <div>
         <h1>Results</h1>
-        {/* <pre>{JSON.stringify(this.props, null, 2)}</pre> */}
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
   }
