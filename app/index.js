2b76deb1f21237a37fe3aaa3740ef82e525e1b59
Mary Kasparian <kasparian.mary@gmail.com>
2022-07-01 14:17:42 -0500

    Install React Router v4, set up Router on index with Route to Popular and Battle component

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Popular from "./components/Popular";
-import "./index.css";
 import Battle from "./components/Battle";
 import Nav from "./components/Nav";
+import "./index.css";
 import { ThemeProvider } from "./contexts/theme";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 // React Component - manages own state, lifecycle methods, renders/returns a React Element (JSX describes what UI looks like)
 class App extends React.Component {
@@ -24,14 +25,19 @@ class App extends React.Component {
   render() {
     // JSX - syntax extension of JS
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
-          <div className="container">
-            <Nav />
-            <Popular />
+      // Router will pass information to child components about the route
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+
+              <Route exact path="/" component={Popular} />
+              <Route path="/battle" component={Battle} />
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </Router>
     );
   }
 }
