03433e32b1b98bf0fef509d63b0191c4016010d2
Mary Kasparian <kasparian.mary@gmail.com>
2022-07-01 14:32:25 -0500

    Add Links to Navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,17 +1,48 @@
 import React from "react";
 import { ThemeConsumer } from "../contexts/theme";
+import { NavLink } from "react-router-dom";
+import { FaMoon, FaRegSun } from "react-icons/fa";
+
+const activeStyle = {
+  color: "rgb(187, 46, 31)",
+};
 
 export default function Nav() {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink
+                to="/"
+                exact
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/battle"
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
           <button
             style={{ fontSize: 30 }}
             className="btn-clear"
             onClick={toggleTheme}
           >
-            {theme == "light" ? "🔦" : "💡"}
+            {theme == "light" ? (
+              <FaMoon size={40} color="rgb(36, 40, 42)" />
+            ) : (
+              <FaRegSun color="#F4B85C" size={40} />
+            )}
           </button>
         </nav>
       )}
